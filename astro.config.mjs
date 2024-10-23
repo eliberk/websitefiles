1e6a36229499e99197e82adaaba4bd5bde0d1624
Eli Berk <eliberk@gmail.com>
2024-10-23 09:20:57 -0400

    More Data Center edits

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
